fe954346a968664d4ace6807540fbb08b2f0b993
user <renars.megnis@inbox.lv>
2018-10-01 14:00:44 +0300

    20181001_14_00

---
+++ test_1.py
@@ -0,0 +1,21 @@
+#!usr/bin/python
+
+a = 10
+b = 1.56
+c = 'W'
+printf("a = %d%(a))
+printf("b = %.4f%(b))
+printf("c = %s%(c))
+
+a = 20
+b = 3.49
+c = '0'
+
+hi
+a = 20
+b = 3.49
+c = '0'
+c = '0'
+# http://www.cplusplus.com/reference/cstdio/printf
+exit ()
+
