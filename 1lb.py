0d7f5a8075bb29c40c4b51ac0dbbc3868f184214
user <renars.megnis@inbox.lv>
2018-11-26 12:44:51 +0200

    11b.py

---
+++ 1lb.py
@@ -0,0 +1,23 @@
+#print(vars())
+import sys
+sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
+#print(vars())
+from numpy import cos, linspace, sin
+#print(vars())
+#x = linspace(0, 7, 70) # solis = (7-0)/(70-1)
+x = linspace(0, 4, 11) # solis = (4-0)/(11-1)
+y = cos(x)
+y2 = sin(x)
+#print(vars())
+from matplotlib import pyplot as plt
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Funkcija $cos(x)$')
+plt.plot(x, y, color = '#FF0000')
+plt.plot(x, y2, color = '#FFf000')
+#plt.show()
+plt.plot(x, y, 'bo' )
+plt.plot(x, y2,'go')
+plt.legend(['$cos(x)$', '$sin(x)$', '$cos(x)$', 'sin(x)'])
+plt.show()
